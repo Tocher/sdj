e86941374ce302b3a2924420bf5f7586e94dc6e2
Sergey Kazakov <tocher@yandex-team.ru>
2016-11-14 01:13:41 +0300

    more

--- sdjkids/gallery_all.php
+++ sdjkids/gallery_all.php
@@ -1,61 +1,33 @@
 <?php include('head.php'); ?>
 <section class="top">
     <div class="top__container">
-        <div class="top__title">Фото</div>
+        <div class="top__title">ФОТО</div>
         <div class="top__subtitle"></div>
-
-            <a href="gallery11.php" class="events__item events__img_item eitem11">
-                <div>Первое командное занятие по диджеингу</div>
-                <div class="events__item-date">12/11/16</div>
-            </a>
-            <a href="gallery1.php" class="events__item events__img_item eitem4">
-                <div>Аргумент</div>
-                <div class="events__item-date">26/10/16</div>
-            </a>
-            <a href="gallery2.php" class="events__item events__img_item eitem3">
-                Аквапарк
-                <div class="events__item-date">28/08/16</div>
-            </a>
-            <a href="gallery6.php" class="events__item events__img_item eitem6">
-                Viva Braslav ZTE
-                <div class="events__item-date">30/07/16</div>
-            </a>
-            <a href="gallery3.php" class="events__item events__img_item eitem1">
-                Зубренок
-                <div class="events__item-date">28/07/16</div>
-            </a>
-            <a href="gallery4.php" class="events__item events__img_item eitem5">
-                Зубренок
-                <div class="events__item-date">24/07/16</div>
-            </a>
-            <a href="gallery5.php" class="events__item events__img_item eitem2">
-                Артек
-                <div class="events__item-date">23/06/13</div>
-            </a>
-            <a href="gallery8.php" class="events__item events__img_item eitem8">
-                Турнир по боулингу
-                <div class="events__item-date">11/12</div>
-            </a>
-            <a href="gallery7.php" class="events__item events__img_item eitem7">
-                Рококо Кидс
-                <div class="events__item-date">10/12</div>
-            </a>
-            <a href="gallery9.php" class="events__item events__img_item eitem9">
-                Одесса
-                <div class="events__item-date">07/12</div>
-            </a>
-            <a href="gallery10.php" class="events__item events__img_item eitem10">
-                Артек
-                <div class="events__item-date">06/12</div>
-            </a>
         <?php
+            $data = array(
+                array('11', 'Первое командное занятие по диджеингу', '12/11/16'),
+                array('1', 'Аргумент', '26/10/16'),
+                array('2', 'Аквапарк', '28/08/16'),
+                array('6', 'Viva Braslav ZTE', '30/07/16'),
+                array('3', 'Зубренок', '28/07/16'),
+                array('4', 'Зубренок', '24/07/16'),
+                array('5', 'Артек', '23/06/13'),
+                array('8', 'Турнир по боулингу', '11/12'),
+                array('7', 'Рококо Кидс', '10/12'),
+                array('9', 'Одесса', '07/12'),
+                array('10', 'Артек', '06/12')
+            );
+
             for ($i = 1; $i <= 11; $i++) {
                 if ($i == 1) {
                     echo '<div class="gallery__row">';
                 }
 
-                echo '<a href="sdjkids/gallery_'.$i.'.php" class="gallery__item">
-                        <img class="lazy" data-original="img/gallery/'.$i.'.jpg">
+                $item = $data[$i-1];
+
+                echo '<a href="sdjkids/gallery_'.$item[0].'.php" class="events__item events__img_item eitem'.$item[0].' gallery__item">
+                        <div>'.$item[1].'</div>
+                        <div class="events__item-date">'.$item[2].'</div>
                     </a>';
 
                 if ($i % 4 == 0) {
--- sdjkids/index2.php
+++ sdjkids/index2.php
@@ -37,11 +37,11 @@
                     <div>Первое командное занятие по диджеингу</div>
                     <div class="events__item-date">12/11/16</div>
                 </a>
-                <a href="gallery1.php" class="events__item events__img_item eitem4">
+                <a href="gallery1.php" class="events__item events__img_item eitem1">
                     <div>Аргумент</div>
                     <div class="events__item-date">26/10/16</div>
                 </a>
-                <a href="gallery2.php" class="events__item events__img_item eitem3">
+                <a href="gallery2.php" class="events__item events__img_item eitem2">
                     Аквапарк
                     <div class="events__item-date">28/08/16</div>
                 </a>
@@ -49,15 +49,15 @@
                     Viva Braslav ZTE
                     <div class="events__item-date">30/07/16</div>
                 </a>
-                <a href="gallery3.php" class="events__item events__img_item eitem1">
+                <a href="gallery3.php" class="events__item events__img_item eitem3">
                     Зубренок
                     <div class="events__item-date">28/07/16</div>
                 </a>
-                <a href="gallery4.php" class="events__item events__img_item eitem5">
+                <a href="gallery4.php" class="events__item events__img_item eitem4">
                     Зубренок
                     <div class="events__item-date">24/07/16</div>
                 </a>
-                <a href="gallery5.php" class="events__item events__img_item eitem2">
+                <a href="gallery5.php" class="events__item events__img_item eitem5">
                     Артек
                     <div class="events__item-date">23/06/13</div>
                 </a>
